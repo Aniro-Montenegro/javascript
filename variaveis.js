750a2dc5939e87ceb309c66ed547178035230734
Aniro <aniromontenegro79@gmail.com>
2021-12-16 14:38:35 -0300

    Variavéis

--- variaveis.js
+++ variaveis.js
@@ -1,5 +1,31 @@
 
 // let var const
 
+//let permite mudar o tipo de dado
 let x=10;
 console.log(x);
+x="oi";
+console.log(x);
+
+/*let x=10;
+console.log(x);
+let x="oi";
+console.log(x);*/
+
+/**
+ * const Não permite mudar o tipo
+ */
+/*const x=10;
+console.log(x);
+x="oi";
+console.log(x);*/
+
+var z=20;
+console.log(z);
+z="bom";
+console.log(z);
+
+var y=20;
+console.log(y);
+var y="bom";
+console.log(y);

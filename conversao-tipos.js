3696cb1e71bef71006c16620e57336bfe241cf42
Aniro <aniromontenegro79@gmail.com>
2021-12-17 15:20:45 -0300

    Conversão de tipos

---
+++ conversao-tipos.js
@@ -0,0 +1,84 @@
+let n1=10;
+
+let n2="2";
+
+//Converte para number e faz o calculo
+console.log(n1 * n2);
+
+//Converte para number e faz o calculo
+console.log(n1 / n2);
+
+//Converte para number e faz o calculo
+console.log(n1 - n2);
+
+//Concatena a string
+console.log(n1 + n2)
+
+
+//parseFloat , parseInt, Number()
+
+
+let v1 ="100"
+console.log(v1,typeof v1);
+v1=parseInt(v1);
+console.log(v1,typeof v1);
+
+
+let c1 ="100.569"
+console.log(c1,typeof c1);
+c1=parseFloat(c1);
+console.log(c1,typeof c1);
+
+let d1 ="110.569"
+console.log(d1,typeof d1);
+d1=parseInt(d1);
+console.log(d1,typeof d1);
+
+let e1 ="120.569jhkjhkjhkjhjkhjk"
+console.log(e1,typeof e1);
+e1=parseInt(e1);
+console.log(e1,typeof e1);
+
+let f1 ="130.569jhkjhkjhkjhjkhjk"
+console.log(f1,typeof f1);
+f1=parseFloat(f1);
+console.log(f1,typeof f1);
+
+let g1 ="140.569jhk5256"
+console.log(g1,typeof g1);
+g1=parseFloat(g1);
+console.log(g1,typeof g1);
+
+let h1 ="hgt150.569jhk5256"
+console.log(h1,typeof h1);
+h1=parseFloat(h1);
+console.log(h1,typeof h1);
+
+
+let i1 ="160"
+console.log(i1,typeof i1);
+i1=Number(i1);
+console.log(i1,typeof i1);
+
+let j1 ="170a"
+console.log(j1,typeof j1);
+j1=Number(j1);
+
+let k1 ="oi";
+let l1=2;
+console.log(k1 * l1,typeof k1,typeof l1);
+
+
+let m1=25;
+console.log(m1.toString(),typeof m1);
+m1=m1.toString();
+console.log(m1.toString(),typeof m1);
+
+let o1=15;
+console.log(o1.toString(),typeof o1);
+o1=o1.toString(16);
+console.log(o1.toString(),typeof o1);
+
+
+
+

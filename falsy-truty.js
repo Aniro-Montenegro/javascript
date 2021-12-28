37b4f11a000437c4955de16f7a2e5310dc8cf384
79Aniro <capablanca1@bol.com.br>
2021-12-28 12:47:40 -0300

    truthy e falsy

---
+++ falsy-truty.js
@@ -0,0 +1,52 @@
+
+//falsy values - 0 ,"", NAN, undefined, null, false
+//Os demais são considerados truty
+
+
+if(0){
+    console.log("true")
+}
+
+else{
+    console.log("false")
+}
+
+
+if(NaN){
+    console.log("true")
+}
+
+else{
+    console.log("false")
+}
+
+if(undefined){
+    console.log("true")
+}
+
+else{
+    console.log("false")
+}
+
+if(null){
+    console.log("true")
+}
+
+else{
+    console.log("false")
+}
+
+if(1){
+    console.log("true")
+}
+
+else{
+    console.log("false")
+}
+if("false"){
+    console.log("true")
+}
+
+else{
+    console.log("false")
+}

52dfcc4a87254420d4342c69f492ca441f1f9417
79Aniro <capablanca1@bol.com.br>
2021-12-28 12:34:34 -0300

    condicionais e operador ternario

---
+++ condicionais-if-else copy.js
@@ -0,0 +1,30 @@
+
+
+
+nota =8
+média=7
+if(nota>=7){
+    console.log("Passou!!!")
+}
+
+else{
+    console.log("Não Passou")
+}
+
+frase= false;
+
+if(frase==true){
+    console.log("Verdade")
+}
+else{
+    console.log("Mentira")
+}
+
+frase= true;
+
+if(!frase){
+    console.log("Mentira")
+}
+else{
+    console.log("Verdade")
+}
---
+++ oparador-ternario.js
@@ -0,0 +1,13 @@
+
+
+
+let nota =5
+let média=7
+
+let resultado="";
+
+resultado= nota>=média ?"Passou": "Não Passou";
+
+console.log(resultado);
+
+

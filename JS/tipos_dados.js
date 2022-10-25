4675975f35074f567114bbef41cf762e18d78aba
Aniro Montenegro <aniro.montenegro@anovasistemas.com.br>
2022-10-25 10:18:16 -0300

    Java Script String e Number

---
+++ JS/tipos_dados.js
@@ -0,0 +1,33 @@
+/*
+String consiste em uma cadeia de caracteres
+Para denotar string no JavaScript são usados aspas duplas ( " " ),
+ aspas simples ( ' ' ) e template literals ou template strings ( ), 
+ template literals permitem textos multilinhas e expressões de linguagem 
+ com os caracteres ${ }, por exemplo: console.log(A soma de duas unidades
+ é ${1+1}), já as outras não, por exemplo: console.log("Isso é um texto").
+*/
+
+console.log('Aniro')
+console.log("Aniro")
+console.log("aspas simples '' ")
+console.log('aspas duplas "" ')
+console.log(`Aspas simples ''\nAspas duplas ""`)
+console.log(`Expressão ${10+20}`)
+console.log(`Donec et metus non mi consectetur consequat. 
+Etiam eu tellus mattis, interdum urna a, interdum tellus. 
+Nam ut porta magna, quis hendrerit ipsum. Nullam malesuada 
+imperdiet bibendum. Ut et ullamcorper massa. Vestibulum ante 
+ipsum primis in faucibus orci luctus et ultrices posuere cubilia 
+curae; Ut congue, diam vel eleifend ultricies, lectus nisi gravida 
+metus, dignissim eleifend ante libero et est. Nunc eros velit, 
+consectetur ut lacinia a, tempus nec erat. Nunc imperdiet laoreet 
+nisl id porttitor. Phasellus blandit, urna commodo tristique euismod, 
+mauris nunc sodales ipsum, nec accumsan arcu diam sed justo.
+ In efficitur, leo non euismod ullamcorper, magna felis faucibus mauris,
+  at efficitur sapien nunc nec mi`)
+
+  /////////////////////////////////////////////////////////////////
+  /*
+  No JavaScript temos 4 tipos de números, os inteiros 
+  (por exemplo: 13), os reais ou float (por exemplo: 83.1),
+   o Not a Number (NaN) e o infinito (Infinity).*/

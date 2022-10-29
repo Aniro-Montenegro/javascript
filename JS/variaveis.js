635a6ff57160171298797a1a03e12b5ffd4ba577
Aniro Montenegro <aniro.montenegro@anovasistemas.com.br>
2022-10-29 11:41:29 -0300

    Variaveis

---
+++ JS/variaveis.js
@@ -0,0 +1,38 @@
+var cidade="Poá"
+console.log(cidade);
+cidade="Mogi"
+console.log(cidade);
+
+let esporte="Futebol"
+console.log(cidade);
+cidade="Handebol"
+console.log(cidade);
+
+const fruta="Banana"
+console.log(fruta)
+//descomente o código para testar
+//fruta="Maça" 
+console.log(fruta)
+
+//Tipod dinamicos
+let palavra=""
+console.log(typeof palavra)
+palavra=10
+console.log(palavra)
+console.log(typeof palavra)
+
+console.log('variavel x existe?',x)
+
+{
+    var x=0
+}
+console.log('variavel x existe?',x)
+
+
+//console.log("variavel y let existe fora do bloco",y) 
+
+{
+let y =10
+console.log("variavel y let existe fora do bloco",y)
+}
+//console.log("variavel y let existe fora do bloco",y) 
